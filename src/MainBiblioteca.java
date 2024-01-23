bd753b42bd00a5c5bfbe32a673d2a60d31569d85
Obruno11 <ikobruno19@lhusurbil.eus>
2024-01-23 10:20:12 +0100

    commit tercero

---
+++ src/MainBiblioteca.java
@@ -0,0 +1,14 @@
+public class MainBiblioteca {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.añadirLibro("El Quijote", "Cervantes", "97-8840806-1052",1560 , "Novela");
+        biblioteca.añadirLibro("Los pilares de la Tierra", "Ken Follett", "97-5440806-1055", 1068, "Novela");
+        biblioteca.añadirLibro("Libro de Poemas", "Federico Garcia Lorca", "97-5440806-1050", 164, "Poesia");
+        biblioteca.mostrarLibros();
+        biblioteca.getNovelas();
+        biblioteca.getPoesia();
+        biblioteca.mostrarLibroMasLargo();
+        biblioteca.eliminarLibro("97-8840806-1052");
+        biblioteca.mostrarLibros();
+    }
+}
